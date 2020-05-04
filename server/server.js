44192495480c55ec7a110e74b3abecdd2f1e58b1
viditchopra1500 <vidit1500@gmail.com>
2020-05-04 21:19:26 +0530

    added GET/todos

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var {Todo}=require('./models/todo');
 var {User}=require('./models/user');
 
 var app=express();
+var port =process.env.PORT ||3000;
 
 app.use(bodyParser.json());
 
@@ -19,12 +20,20 @@ app.post('/todos',(req,res)=>{
     }).catch((e)=>{
         res.status(400).send(e);
     })
+});
+
+app.get('/todos',(req,res)=>{
+    Todo.find().then((docs)=>{
+        res.send({docs})
+    }).catch((e)=>{
+        res.status(400).send(e);
+    })
 })
 
 
 
-app.listen(3000,()=>{
-    console.log('started on port 3000');
+app.listen(port,()=>{
+    console.log(`started on port ${port}`);
 })
 
 module.exports={
